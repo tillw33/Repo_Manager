1471982e9e3a22306674cfc45afbcd4f3e43bf25
Till <till.weithoff@gmail.com>
2023-01-02 15:21:36 +0100

    first iteration

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import sys
 from tool_class import tool_class
 import os
 
-list_f = "./repos.txt"
-repo_list = open(list_f).read().split("\n")
+""" database directory """
+tool_dir = os.path.dirname(__file__)
+db_folder = os.path.join(tool_dir, "database")
+if not os.path.isdir(db_folder):
+    os.mkdir(db_folder)
+database = os.path.join(db_folder, "repos.txt")
+
+try:
+    repo_list = open(database).read().split("\n")
+    repo_list = list(filter(None, repo_list))
+except:
+    repo_list = []
 def tool_setup():
     t = tool_class.tool(name='Repo Manager')
     # description
@@ -13,13 +23,55 @@ def tool_setup():
                     Used now to pull all updates for given repos
                     """)
     # required args
+    t.add_required("action", "requested action")
 
     # optional args
 
     # default args
+    t.add_default("action")
     return t
+
+def pull_repos(t):
+    if len(repo_list) == 0:
+        t.msg("No repos to pull in database")
+        t.msg("Aborting...")
+        exit()
+    for repo in repo_list:
+        if "~" in repo:
+            r_path = os.path.expanduser(repo)
+        else:
+            r_path = repo
+        t.msg("Pulling rep: "+ repo)
+        os.chdir(r_path)
+        out = os.popen("git pull").read()
+        t.msg("git output : "+ out)
+
+def add_repo(t):
+    cwd = os.getcwd()
+    t.msg("Appending "+cwd)
+    repo_list.append(cwd)
+    f = open(list_f,"w")
+    f.write("\n".join(repo_list))
+    f.close()
+
+def rem_repo(t):
+    cwd = os.getcwd()
+    t.msg("Removing "+cwd)
+    repo_list.remove(cwd)
+    f = open(list_f,"w")
+    f.write("\n".join(repo_list))
+    f.close()
+
 def run(t):
-    print(repo_list)
+    if t.args.action == "pull":
+        pull_repos(t)
+    elif t.args.action == "add":
+        add_repo(t)
+    elif t.args.action == "rm":
+        rem_repo(t)
+    else:
+        t.msg("Unknown action, aborting...")
+        exit()
 
 def main(args):
     t = tool_setup()
